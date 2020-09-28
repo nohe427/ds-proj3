77bc610aa43ecba089a1e6c2d11db53fc84062cf
Alexander Nohe <nohe427@users.noreply.github.com>
2020-09-28 17:02:48 +0000

    Problem two solved.
    Problem two is solved with edge cases written.

--- problem_2.py
+++ problem_2.py
@@ -7,7 +7,35 @@ def rotated_array_search(input_list, number):
     Returns:
        int: Index or -1
     """
-   pass
+    if len(input_list) < 1:
+        return -1
+
+    lower = 0
+    upper = len(input_list)-1
+
+    return rotated_array_search_helper(input_list, lower, upper, number)
+
+def rotated_array_search_helper(arr, lower_index, upper_index, number):
+    mid_index = (lower_index + upper_index)//2
+
+    if arr[mid_index] == number:
+        return mid_index
+    if lower_index >= mid_index:
+        return -1
+
+    # This is the sorted array pattern
+    if arr[lower_index] < arr[upper_index]:
+        # Exit early condition.
+        if number not in range(arr[lower_index], arr[upper_index]):
+            return -1
+        if number > arr[mid_index]:
+            return rotated_array_search_helper(arr, mid_index, upper_index, number)
+        else:
+            return rotated_array_search_helper(arr, lower_index, mid_index, number)
+    
+    # This is the rotated array pattern
+    if arr[lower_index] > arr[upper_index]:
+        return max(rotated_array_search_helper(arr, lower_index, mid_index, number), rotated_array_search_helper(arr, mid_index, upper_index, number))
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):
@@ -28,3 +56,12 @@ test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
 test_function([[6, 7, 8, 1, 2, 3, 4], 8])
 test_function([[6, 7, 8, 1, 2, 3, 4], 1])
 test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+
+# Edge case 1 - A fully rotated array
+test_function([[10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 9])
+
+# Edge case 2 - an Array with only one element
+test_function([[2], 2])
+
+# Edge case 3 - an Array with only zero elements
+test_function([[], 2])
